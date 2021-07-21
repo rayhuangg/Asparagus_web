0db7063ac16717d6c3ddadbc5635aa1a3c943ce6
JustinBear99 <justin850509@gmail.com>
2021-07-21 23:21:32 +0800

    view by sections

--- stats/views.py
+++ stats/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import Q
 import sys
 from os import path
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
@@ -14,45 +15,75 @@ def index(request):
     sections = [ s for s in Section.objects.all()]
     return render(request, 'stats/stats.html', context={'demolist': demos[::-1], 'demorange': demo_range[::-1], 'sections': sections})
 
+# def checkRange(request):
+#     if request.method == 'POST':
+#         fromValue = request.POST['fromValue']
+#         untilValue = request.POST['untilValue']
+#         section = int(request.POST['section'])
+#         demolist = [ d for d in Demo.objects.all() if int(fromValue) <= d.id <= int(untilValue)]
+#         # print(fromValue, untilValue, demolist)
+#         labels = [ d.name for d in demolist ]
+#         clumps, stalks, spears = [], [], []
+#         if section == 0:
+#             for demo in demolist:
+#                 name_demo = demo.name
+#                 num_clump, num_stalk, num_spear = 0, 0, 0
+#                 for result in ResultList.objects.filter(demo__id=demo.id):
+#                     name_result = result.name
+#                     for instance in Instance.objects.filter(resultlist__id=result.id):
+#                         if instance.predicted_class == 'clump':
+#                             num_clump += 1
+#                         elif instance.predicted_class == 'stalk':
+#                             num_stalk += 1
+#                         elif instance.predicted_class == 'spear':
+#                             num_spear += 1
+#                 clumps.append(num_clump)
+#                 stalks.append(num_stalk)
+#                 spears.append(num_spear)
+#         else:
+#             for demo in demolist:
+#                 name_demo = demo.name
+#                 num_clump, num_stalk, num_spear = 0, 0, 0
+#                 for result in ResultList.objects.filter(demo__id=demo.id).filter(image__section__id=section):
+#                     name_result = result.name
+#                     for instance in Instance.objects.filter(resultlist__id=result.id):
+#                         if instance.predicted_class == 'clump':
+#                             num_clump += 1
+#                         elif instance.predicted_class == 'stalk':
+#                             num_stalk += 1
+#                         elif instance.predicted_class == 'spear':
+#                             num_spear += 1
+#                 clumps.append(num_clump)
+#                 stalks.append(num_stalk)
+#                 spears.append(num_spear)
+#         return HttpResponse(json.dumps({'labels': labels[::-1], 'clumps': clumps[::-1], 'stalks': stalks[::-1], 'spears': spears[::-1]}))
+
 def checkRange(request):
     if request.method == 'POST':
         fromValue = request.POST['fromValue']
         untilValue = request.POST['untilValue']
-        section = int(request.POST['section'])
         demolist = [ d for d in Demo.objects.all() if int(fromValue) <= d.id <= int(untilValue)]
-        # print(fromValue, untilValue, demolist)
-        labels = [ d.name for d in demolist ]
-        clumps, stalks, spears = [], [], []
-        if section == 0:
-            for demo in demolist:
-                name_demo = demo.name
-                num_clump, num_stalk, num_spear = 0, 0, 0
-                for result in ResultList.objects.filter(demo__id=demo.id):
-                    name_result = result.name
-                    for instance in Instance.objects.filter(resultlist__id=result.id):
-                        if instance.predicted_class == 'clump':
-                            num_clump += 1
-                        elif instance.predicted_class == 'stalk':
-                            num_stalk += 1
-                        elif instance.predicted_class == 'spear':
-                            num_spear += 1
-                clumps.append(num_clump)
-                stalks.append(num_stalk)
-                spears.append(num_spear)
-        else:
-            for demo in demolist:
-                name_demo = demo.name
-                num_clump, num_stalk, num_spear = 0, 0, 0
-                for result in ResultList.objects.filter(demo__id=demo.id).filter(image__section__id=section):
-                    name_result = result.name
-                    for instance in Instance.objects.filter(resultlist__id=result.id):
-                        if instance.predicted_class == 'clump':
-                            num_clump += 1
-                        elif instance.predicted_class == 'stalk':
-                            num_stalk += 1
-                        elif instance.predicted_class == 'spear':
-                            num_spear += 1
-                clumps.append(num_clump)
-                stalks.append(num_stalk)
-                spears.append(num_spear)
-        return HttpResponse(json.dumps({'labels': labels[::-1], 'clumps': clumps[::-1], 'stalks': stalks[::-1], 'spears': spears[::-1]}))
+        resultlist = []
+        for demo in demolist:
+            for re in ResultList.objects.filter(demo__id=demo.id):
+                resultlist.append(re)
+        clumps, stalks, spears = {}, {}, {}
+
+        for result in resultlist:
+            name_section = result.image.section.name
+            instances = Instance.objects.filter(resultlist__id=result.id)
+            if name_section in spears:
+                pass
+            else:
+                clumps[name_section] = 0
+                stalks[name_section] = 0
+                spears[name_section] = 0
+            for instance in instances:
+                if instance.predicted_class == 'clump':
+                    clumps[name_section] += 1
+                elif instance.predicted_class == 'stalk':
+                    stalks[name_section] += 1
+                elif instance.predicted_class == 'spear':
+                    spears[name_section] += 1
+        
+        return HttpResponse(json.dumps({'clumps': clumps, 'stalks': stalks, 'spears': spears}))
