3599122b23acc87b28971c40439867b5fc0ca8ba
JustinBear99 <justin850509@gmail.com>
2021-08-19 12:21:00 +0800

    Add foreignkey with frontview

--- lidar/models.py
+++ lidar/models.py
@@ -4,11 +4,12 @@ from datetime import datetime
 import sys
 from os import path
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
-from record.models import ImageList
+from record.models import ImageList, FrontView
 
 class Scan(models.Model):
     name = models.CharField(max_length=100, default=str(datetime.now().strftime('%Y%m%d_%H%M%S')))
     date = models.DateTimeField('Date created', default=now)
+    front_image = models.ForeignKey(FrontView, on_delete=models.CASCADE)
     left_image = models.ForeignKey(ImageList, on_delete=models.CASCADE, related_name='image_from_left_side')
     right_image = models.ForeignKey(ImageList, on_delete=models.CASCADE, related_name='image_from_right_side')
     points = models.JSONField()
