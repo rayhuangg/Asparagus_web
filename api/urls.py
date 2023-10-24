f4ca7dfcbe4f9515f16f8f7481741f42d2e9929f
RayHuang <ray00211@gmail.com>
2023-10-24 22:53:56 +0800

    Add batch search function

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 # api/urls.py
 from django.urls import path
-from .views import  ResultListView
+from .views import  ResultListView, BatchSearchView
 
 urlpatterns = [
     path('section/<str:sectionName>/', ResultListView.as_view(), name='sectionName'),
+    path('batch/<int:batchId>/', BatchSearchView.as_view(), name='batchId')
 ]
--- api/views.py
+++ api/views.py
@@ -23,12 +23,7 @@ class ResultListView(generics.ListAPIView):
             predicted_class="stalk"
         ).count()
 
-        if stalk_count > 25:
-            density = "high"
-        elif 15 <= stalk_count <= 25:
-            density = "medium"
-        elif stalk_count < 15:
-            density = "low"
+        density = determine_density(stalk_count)
 
         # Build the response data
         response_data = {
@@ -38,3 +33,46 @@ class ResultListView(generics.ListAPIView):
         }
 
         return Response(response_data)
+
+
+class BatchSearchView(generics.ListAPIView):
+    serializer_class = ResultListSerializer
+
+    def get_queryset(self):
+        batch_id = self.kwargs['batchId']
+        resultlists = ResultList.objects.filter(demo__id=batch_id)
+        return resultlists
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        sections = []
+        result_data = []
+
+        for resultlist in queryset:
+            sections.append(resultlist.image.section.name)
+
+        result_data = {}
+        for section in sections:
+            resultlist_per_section = queryset.filter(image__section__name=section)[0]
+            stalk_count = Instance.objects.filter(resultlist_id=resultlist_per_section.id, predicted_class="stalk").count()
+            density = determine_density(stalk_count)
+
+            result_data[section] = {
+                "predict_time": resultlist_per_section.date.strftime("%Y-%m-%d %H:%M:%S"),
+                "stalk_count": stalk_count,
+                "density": density
+            }
+
+
+        return Response(result_data)
+
+
+def determine_density(stalk_count):
+    if stalk_count > 25:
+        density = "high"
+    elif 15 <= stalk_count <= 25:
+        density = "medium"
+    elif stalk_count < 15:
+        density = "low"
+
+    return density
