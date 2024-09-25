dc18799e5c252439ed46a8ae94d5c0f473d58e0a
RayHuang <ray00211@gmail.com>
2024-09-25 15:21:49 +0800

    Modify front end fertilizer history display

--- spray/serializers.py
+++ spray/serializers.py
@@ -6,27 +6,35 @@ from django.utils.timezone import now
 class FertilizerUsageSerializer(serializers.ModelSerializer):
     fertilizer_id = serializers.PrimaryKeyRelatedField(queryset=FertilizerList.objects.all(), source='fertilizer', write_only=True)
     amount = serializers.FloatField()
+    fertilizer_name = serializers.CharField(source='fertilizer.name', read_only=True)
 
     class Meta:
         model = FertilizerUsage
-        fields = ['fertilizer_id', 'amount']
+        fields = ['fertilizer_id', 'amount', 'fertilizer_name']
 
 
 class SprayExperimentRecordSerializer(serializers.ModelSerializer):
     end_time = serializers.DateTimeField(required=False, allow_null=True)
-    fertilizers = FertilizerUsageSerializer(many=True, write_only=True)  # 只在 "開始" 時寫入藥肥使用量
-    total_water_used = serializers.FloatField(required=False, allow_null=True)  # 僅在 "結束" 時記錄總用水量
+    fertilizer_usages = FertilizerUsageSerializer(many=True, read_only=True)
+    total_water_used = serializers.FloatField(required=False, allow_null=True)
+    fertilizer_usage_summary = serializers.SerializerMethodField()
 
     class Meta:
         model = SprayExperimentRecord
-        fields = ['location', 'greenhouse', 'fertilizers', 'total_water_used', 'start_time', 'end_time', 'note']
+        fields = ['location', 'greenhouse', 'fertilizer_usages', 'total_water_used', 'start_time', 'end_time', 'note', 'fertilizer_usage_summary']
+
+    # 定義方法來返回模型中的 `get_fertilizer_usage` 結果
+    def get_fertilizer_usage_summary(self, obj):
+        return obj.get_fertilizer_usage()
+
 
     def create(self, validated_data):
-        fertilizers_data = validated_data.pop('fertilizers')  # 從資料中取出 fertilizers
-        experiment = SprayExperimentRecord.objects.create(**validated_data)  # 創建實驗記錄
+        fertilizers_data = self.context['request'].data.get('fertilizers', [])
+        experiment = SprayExperimentRecord.objects.create(**validated_data)
         for fertilizer_data in fertilizers_data:
-            FertilizerUsage.objects.create(experiment=experiment, **fertilizer_data)  # 創建每種藥肥的使用記錄
+            FertilizerUsage.objects.create(experiment=experiment, fertilizer_id=fertilizer_data['id'], amount=fertilizer_data['amount'])
         return experiment
+
 
     def update(self, instance, validated_data):
         # Implement similar logic for update if necessary
